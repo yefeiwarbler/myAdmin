980a605bd19cdd79d4927da6fcc5132ccbdfeb3f
yefeiwarbler <yefei0312@163.com>
2020-11-06 13:48:34 +0800

    add component PageTitle

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: "PageTitle",
+    props: {
+        pageTitle: "",
+    },
+    created(){
+        document.title = this.pageTitle;
+    }
+}
+</script>
+
+<style>
+
+</style>
