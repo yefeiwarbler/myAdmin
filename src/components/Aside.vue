35a1e34a05a825ce07e018dc55e3c10714248804
yefeiwarbler <yefei0312@163.com>
2020-11-06 13:53:04 +0800

    change aside's stylesheet and update links

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,63 +1,62 @@
 <template>
     <el-aside
         width="200px"
+        class="main-aside"
     >
         <el-menu
             background-color="#1e222d"
             text-color="#c1c6c8"
             active-text-color="#fff"
+            :default-active="this.$route.path"
+            router
         >
-            <el-submenu index="1">
+            <el-menu-item index="/" route="/">
+                <i class="el-icon-s-home"></i>
+                <span slot="title">首页</span>
+            </el-menu-item>
+            <el-submenu index="2">
                 <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span>导航一</span>
+                    <i class="el-icon-tickets"></i>
+                    <span>商品</span>
                 </template>
-                <el-menu-item-group>
-                    <template slot="title">分组一</template>
-                    <el-menu-item index="1-1">选项1</el-menu-item>
-                    <el-menu-item index="1-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                    <el-menu-item-group title="分组2">
-                    <el-menu-item index="1-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="1-4">
-                    <template slot="title">选项4</template>
-                    <el-menu-item index="1-4-1">选项1</el-menu-item>
-                </el-submenu>
+                <el-menu-item index="/product" route="/product">商品管理</el-menu-item>
+                <el-menu-item index="/test" route="/test">品类管理</el-menu-item>
             </el-submenu>
-            <el-menu-item index="2">
-                <i class="el-icon-s-home"></i>
-                <span slot="title">首页</span>
-            </el-menu-item>
-            <el-menu-item index="3">
-                <i class="el-icon-s-home"></i>
-                <span slot="title">首页</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-                <i class="el-icon-s-home"></i>
-                <span slot="title">首页</span>
-            </el-menu-item>
-            <el-menu-item index="5">
-                <i class="el-icon-s-home"></i>
-                <span slot="title">首页</span>
-            </el-menu-item>
-            <el-menu-item index="6">
-                <i class="el-icon-s-home"></i>
-                <span slot="title">首页</span>
-            </el-menu-item>
+            <el-submenu index="3">
+                <template slot="title">
+                    <i class="el-icon-s-operation"></i>
+                    <span slot="title">订单</span>
+                </template>
+                <el-menu-item index="/order" route="/order">订单管理</el-menu-item>
+            </el-submenu>
+            <el-submenu index="4">
+                <template slot="title">
+                    <i class="el-icon-user"></i>
+                    <span slot="title">用户</span>
+                </template>
+                <el-menu-item index="4-1" route="/user">用户管理</el-menu-item>
+            </el-submenu>
         </el-menu>
     </el-aside>
 </template>
 
 <script>
-
 export default {
     name: "Aside",
-    components: {
-    }
 }
 </script>
 
 <style lang="scss">
+    .main-aside{
+        position: fixed;
+        height: 100%;
 
+        .el-menu{
+            height: 100%;
+        }
+
+        .el-submenu .el-menu-item{
+            min-width: unset;
+        }
+    }
 </style>
